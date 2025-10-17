3bbf15bc353915882fc73941bcc35df974fab95d
Stoble <stoble@mail.ru>
2025-10-17 21:35:53 +0300

    chore: add react-query to the project

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,11 +1,14 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ReactNode } from "react";
 import AntdConfigProvider from "./AntdConfigProvider";
+import QueryProvider from "./QueryProvider";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <AntdRegistry>
-      <AntdConfigProvider>{children}</AntdConfigProvider>
+      <AntdConfigProvider>
+        <QueryProvider>{children}</QueryProvider>
+      </AntdConfigProvider>
     </AntdRegistry>
   );
 };
---
+++ src/app/providers/QueryProvider.tsx
@@ -0,0 +1,12 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+import queryClient from "@/shared/api/queryClient";
+
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
